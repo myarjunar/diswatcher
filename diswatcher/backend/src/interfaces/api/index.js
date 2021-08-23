bbe6350d1bb4ab95f99d460b19b875817adc8298
myarjunar <myarjunar@gmail.com>
2021-08-23 20:05:15 +0700

    uncomment fetching pbf file from Dockerfile

--- diswatcher/backend/src/interfaces/api/index.js
+++ diswatcher/backend/src/interfaces/api/index.js
@@ -5,7 +5,7 @@ const helmet = require('helmet');
 const swaggerUi = require('swagger-ui-express');
 
 const app = express();
-const swaggerDocument = require('../../../docs/openapi.json');
+// const swaggerDocument = require('../../../docs/openapi.json');
 const logger = require('../../helpers/logger');
 
 const port = process.env.PORT || 8010;
@@ -16,7 +16,7 @@ async function init() {
 
   require('./routes/index')(app);
 
-  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  // app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.listen(port, () =>
     logger.info(`App started and listening on port ${port}`),
